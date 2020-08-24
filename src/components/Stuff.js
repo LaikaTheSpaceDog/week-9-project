52ff359a1ce8e387a7eba61f553622a9512dd590
Oscar Wales <oscarjwales@gmail.com>
2020-08-24 16:51:30 +0100

    Added prop to paragraph component

--- src/components/Stuff.js
+++ src/components/Stuff.js
@@ -7,7 +7,7 @@ import Square from "./Square.js"
 const Stuff = () => (
     <>
       <Header text="Hello, world!"/>
-      <Paragraph />
+      <Paragraph message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos."/>
       <Square />
       <People />
     </>
