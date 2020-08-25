8b98bb4096d3ecb36a0e839c2323e6dde6cfcf90
Oscar Wales <oscarjwales@gmail.com>
2020-08-25 14:55:15 +0100

    Completed state challenge to update square component to change colour on click

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Square = props => (
-    <div style={{backgroundColor: `${props.colour}`, width: 200, height: 200 }}></div>
-);
+class Square extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            toggled: false,
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+
+    }
+
+    handleClick() {
+        let current = this.state.toggled;
+        this.setState({toggled: !current})
+    }
+
+    render() {
+        let { colour } = this.props;
+
+        return (
+            <>
+            {this.state.toggled === false ? <div style={{backgroundColor: "green", width: 200, height: 200 }} onClick={ this.handleClick }></div> :
+            <div style={{backgroundColor: `${colour}`, width: 200, height: 200 }} onClick={ this.handleClick }></div>}
+            </>
+        );
+    }
+}
 
 Square.defaultProps = {
-    colour: "black"
+    colour: "green"
 }
 
 export default Square;
