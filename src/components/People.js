7f6c9b537378aea90fec0dcc8e3aceb907c3c0e9
Oscar Wales <oscarjwales@gmail.com>
2020-08-25 09:58:09 +0100

    Finished adding ternary operator to people component

--- src/components/People.js
+++ src/components/People.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
-const People = props => (
-    <ul className="list-group">
-        { props.names.map((value, index) => (
-            <li className="list-group-item" key={ index }>
-               { value }
-            </li>
-        )) }
-    </ul>
+const People = ({names}) => (
+    <>
+        {!names ? <p>Nobody here</p> : 
+            <ul className="list-group">
+                { names.map((value, index) => (
+                    <li className="list-group-item" key={ index }>
+                    { value }
+                    </li>
+                ))}
+            </ul>
+        }
+    </>
+
 );
 
-People.defaultProps = {
-    names: ["Nobody here!"]
-}
+// People.defaultProps = {
+//     names: ["Nobody here!"]
+// }
 
 export default People;
