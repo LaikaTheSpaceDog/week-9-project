e17dc231a3c6cd97b24a8717bfd22b0c0e60d8b7
Oscar Wales <oscarjwales@gmail.com>
2020-08-27 15:12:55 +0100

    Added a 404 page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FourOhFour from './components/FourOhFour.js';
 import Stuff from "./components/Stuff.js";
 import Header from "./components/Header.js";
 import Content from "./components/Content.js"
@@ -7,34 +8,38 @@ import PasswordStrength from "./components/PasswordStrength.js"
 import Basket from "./components/Basket.js"
 import CatchMeIfYouCan from "./components/CatchMeIfYouCan.js"
 import Footer from "./components/Footer.js"
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 
 const App = () => (
   <Router>
     <Header>What an amazing website!</Header>
-    <Route exact path="/">
-      <Stuff />
-    </Route>
-    <Route exact path="/content">
-      <Content />
-    </Route>
-    <Route exact path="/god">
-      <GodCounter />
-    </Route>
-    <Route exact path="/password">
-      <PasswordStrength label={ "Password" } name={ "password" }/>
-    </Route>
-    <Route exact path="/basket">
-      <Basket items={ [
-          { name: "Coffee", price: 2.10 },
-          { name: "Bananas", price: 3.50 },
-          { name: "Milk", price: 250.65 },
-          { name: "The Great Milk Shortage by Simon Schama", price: 12.99 },
-          ] } />
-    </Route>
-    <Route exact path="/catch">
-      <CatchMeIfYouCan jump={ 100 } />
-    </Route>
+    <Switch>
+      <Route exact path="/">
+        <Stuff />
+      </Route>
+      <Route exact path="/content">
+        <Content />
+      </Route>
+      <Route exact path="/god">
+        <GodCounter />
+      </Route>
+      <Route exact path="/password">
+        <PasswordStrength label={ "Password" } name={ "password" }/>
+      </Route>
+      <Route exact path="/basket">
+        <Basket items={ [
+            { name: "Coffee", price: 2.10 },
+            { name: "Bananas", price: 3.50 },
+            { name: "Milk", price: 250.65 },
+            { name: "The Great Milk Shortage by Simon Schama", price: 12.99 },
+            ] } />
+      </Route>
+      <Route exact path="/catch">
+        <CatchMeIfYouCan jump={ 100 } />
+      </Route>
+      <FourOhFour />
+    </Switch>
     <Footer />
   </Router>
   );
---
+++ src/components/FourOhFour.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FourOhFour=() => (
+
+    <h2>404: Here be Dragons</h2>
+
+);
+
+export default FourOhFour;
