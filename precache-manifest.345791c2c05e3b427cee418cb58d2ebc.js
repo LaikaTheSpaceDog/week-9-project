05189c1f45598ca0be6024c3eb6bf82b3055b184
Oscar Wales <oscarjwales@gmail.com>
2020-08-28 11:52:35 +0100

    Updates

---
+++ precache-manifest.345791c2c05e3b427cee418cb58d2ebc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3795c6b6e95f8dd681dcf807ac341a",
+    "url": "/LaikaTheSpaceDog/week-9-project/index.html"
+  },
+  {
+    "revision": "9f5da70e8e77f353ca4e",
+    "url": "/LaikaTheSpaceDog/week-9-project/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "bbb5e5f49606cf99e589",
+    "url": "/LaikaTheSpaceDog/week-9-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9f5da70e8e77f353ca4e",
+    "url": "/LaikaTheSpaceDog/week-9-project/static/js/2.a11f3cfa.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/LaikaTheSpaceDog/week-9-project/static/js/2.a11f3cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb5e5f49606cf99e589",
+    "url": "/LaikaTheSpaceDog/week-9-project/static/js/main.9e31a196.chunk.js"
+  },
+  {
+    "revision": "655121df8c13e1e2810c",
+    "url": "/LaikaTheSpaceDog/week-9-project/static/js/runtime-main.f8d9f62e.js"
+  }
+]);
